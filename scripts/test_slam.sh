34e1407ac4ada44540ecc2127132cfa8da763b31
lonoty <40420163+lonoty@users.noreply.github.com>
2020-02-09 05:46:17 -0800

    Add files via upload

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e "cd $(pwd)/../../..; source devel/setup.bash ; roslaunch my_robot world.launch " &
+xterm  -e "cd $(pwd)/..; source devel/setup.bash ; roslaunch my_robot world.launch " &
 sleep 5
-xterm  -e "cd $(pwd)/../../..; source devel/setup.bash ; rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
+xterm  -e "cd $(pwd)/..; source devel/setup.bash ; rosrun teleop_twist_keyboard teleop_twist_keyboard.py  " &
 sleep 2
-xterm -e "cd $(pwd)/../../..; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
+xterm -e "cd $(pwd)/..; source devel/setup.bash ; roslaunch my_robot mapping.launch " &
 sleep 3
-xterm  -e "cd $(pwd)/../../..; source devel/setup.bash ; rosrun rviz rviz -d $(pwd)/../rvizConfig/slamconf.rviz" &
+xterm  -e "cd $(pwd)/..; source devel/setup.bash ; rosrun rviz rviz -d $(pwd)/../rvizConfig/slamconf.rviz" &
