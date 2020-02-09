b9bb978ceb78b42d44c64cc2a7ef9c76750c726f
lonoty <40420163+lonoty@users.noreply.github.com>
2020-02-09 04:28:26 -0800

    Add files via upload

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch my_robot world.launch " &
+xterm  -e "cd $(pwd)/../../..; source devel/setup.bash ; roslaunch my_robot world.launch " &
 sleep 5
-xterm -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch my_robot amcl.launch  " &
+xterm -e "cd $(pwd)/../../..; source devel/setup.bash ; roslaunch my_robot amcl.launch  " &
 sleep 3
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun rviz rviz -d $(pwd)/../rvizConfig/homeserviceconf.rviz" &
+xterm  -e "cd $(pwd)/../../..; source devel/setup.bash ; rosrun rviz rviz -d $(pwd)/../rvizConfig/homeserviceconf.rviz" &
 sleep 5
-xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun pick_objects pick_objects_w_markers  " &
+xterm -e "cd $(pwd)/../../..; source devel/setup.bash ; rosrun pick_objects pick_objects_w_markers  " &
